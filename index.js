dd77fc29a5998d3576a2d5f71e64238f21e5c197
Ingwie Phoenix <ingwie2000@googlemail.com>
2015-05-12 17:50:45 +0200

    Added ability for user to pass options via query parameter.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var ejs = require('ejs'),
-  uglify = require('uglify-js');
+  uglify = require('uglify-js'),
+  utils = require("loader-utils");
 
 
 module.exports = function (source) {
   this.cacheable && this.cacheable();
-  var template = ejs.compile(source, {
-    client: true,
-    filename: '.'
-  });
+  var opts = utils.parseQuery(this.query);
+  opts.client = true;
+  opts.filename = this.resourcePath;
+  var template = ejs.compile(source, opts);
 
   var ast = uglify.parser.parse(template.toString());
 
