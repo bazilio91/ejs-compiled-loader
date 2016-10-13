0e6a478e6a6c4016ce6b719c3d085f4773ddec07
Lex Robinson <lex.robinson@smarkets.com>
2016-10-13 18:37:48 +0100

    Fix tests
    All the template files end with a newline as of c7dca60, so the asserts
    need to check for them.

--- test/app.js
+++ test/app.js
@@ -1,13 +1,13 @@
 const assert = require('assert');
 
 var tpl = require("./template.ejs");
-assert.equal(tpl({noun: "World"}), 'Hello, World!');
+assert.equal(tpl({noun: "World"}), 'Hello, World!\n');
 
 var tpl3 = require("./subdir/parent.ejs");
-assert.equal(tpl3({foo: "foo"}), "parent: child: foo\n");
+assert.equal(tpl3({foo: "foo"}), "parent: child: foo\n\n");
 
 var tpl4 = require("./htmlmin.ejs");
-assert.equal(tpl4({test: 123}), '123');
+assert.equal(tpl4({test: 123}), '123\n');
 
 var tpl2 = require("!!../?delimiter=?!./template2.ejs");
 assert.equal(tpl2({hobbies: ["food", "code"]}).trimRight(), "  I like food.\n  I like code.");
